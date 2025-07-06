6c99021c0d33fdbf527878fe3ecef530341d5a27
Martín Mena <menamartinjj@gmail.com>
2025-07-06 12:21:38 -0300

    AddFooterJSX

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,29 @@ import Cart from './components/Cart';
 import Checkout from './components/Checkout';
 import './App.css';
 import './index.css';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ItemListContainer greeting="¡Bienvenido a Mena Store!" />} />
-          <Route path="/category/:categoryId" element={<ItemListContainer greeting="Filtrado por categoría" />} />
-          <Route path="/item/:id" element={<ItemDetailContainer />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <div id="root" className="d-flex flex-column min-vh-100">
+        <div className="app-container flex-grow-1">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<ItemListContainer greeting="¡Bienvenido a Mena Store!" />} />
+            <Route path="/category/:categoryId" element={<ItemListContainer greeting="Filtrado por categoría" />} />
+            <Route path="/item/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => (
+  <footer className="bg-dark text-white d-flex justify-content-center align-items-center" style={{ height: '100px' }}>
+    Mena Martín -&nbsp;
+    <a
+      href="https://www.coderhouse.com.ar"
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ color: 'white', textDecoration: 'none', margin: '0 5px' }}
+    >
+      Coderhouse
+    </a>
+    - ReactJS - 2025
+  </footer>
+);
+
+export default Footer;
